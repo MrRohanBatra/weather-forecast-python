0f25b2044de6d1b6da5137f023e424e1df6ac303
Rohan Batra <rohanbatra.in@gmail.com>
2023-11-02 14:04:14 +0530

    Createweather.py

---
+++ weather.py
@@ -0,0 +1,12 @@
+import requests
+API="cb6b65b6dc8c47cb9bf124135230111"
+def weather_forcast(city,Api):
+    baseurl="http://api.weatherapi.com/v1/current.json"
+    p={'key':Api,'q':city}
+    fetch_data=requests.get(baseurl,params=p)
+    if (fetch_data.status_code ==200):
+        data=fetch_data.json()
+        temp_c=data["current"]["temp_c"]
+        temp_f=data["current"]["temp_f"]
+        loaction=data["location"]["name"]+","+data["location"]["country"]
+        return {'loc':loaction,'celcious'=temp_c,"faren":temp_c}
