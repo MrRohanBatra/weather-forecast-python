ac8378cdbfe48595ebedf52b5c9ab95f052403d5
Rohan Batra <rohanbatra.in@gmail.com>
2023-11-02 21:58:31 +0530

    Update weather.py

--- weather.py
+++ weather.py
@@ -1,16 +1,38 @@
 import requests
-from datetime import *
-API="cb6b65b6dc8c47cb9bf124135230111"
+import os
+import py_setenv as pyset
+from datetime import * 
+if "API_Weatherapi.com" in os.environ:
+    API=os.getenv("API_weatherapi.com")
+else:
+    API=input("Enter your API key: ")
+    pyset.set_variable("API_Weatherapi.com",API,os.getenv("USERNAME"))   # to set environment variable
 city=input("Enter city name in the format (city,name)\nAns:")
 def weather_forcast(city,Api):
     baseurl="http://api.weatherapi.com/v1/current.json"
-    p={'key':Api,'q':city}
+    p={'key':Api,'q':city,'aqi':"yes"}
     fetch_data=requests.get(baseurl,params=p)
     if (fetch_data.status_code ==200):
         data=fetch_data.json()
         temp_c=data["current"]["temp_c"]
         temp_f=data["current"]["temp_f"]
         time=datetime.now().time().strftime("%H:%M")
-        loaction=data["location"]["name"]+","+data["location"]["country"]
-        print(f"\nThe weather for the day is..................\nLocation:{loaction}\nTemperature:{temp_c}°C || {temp_f}°F\nDate:{datetime.now().date()} {time}")
-weather_forcast(city,API)
+        aqi=round(float(str(data["current"]["air_quality"]["co"])[1:]))
+        location=data["location"]["name"]+","+data["location"]["country"]
+        if (aqi in range(0,50)):
+            aqi_condition="Good"
+        elif(aqi in range(50,100)):
+            aqi_condition="Satisfactory"
+        elif(aqi in range(100,200)):
+            aqi_condition="Moderately Polluted"
+        elif(aqi in range(200,300)):
+            aqi_condition="Poor"
+        elif(aqi in range(300,400)):
+            aqi_condition="Very Poor"
+        else:
+            aqi_condition="Serve"
+        print(f"\nThe weather for the day is..................\n->Location:{location}\n->Temperature:{temp_c}°C || {temp_f}°F\n->AQI:{aqi} {aqi_condition}\n->Date:{datetime.now().date()} {time}\n\n")
+    else:
+        print("\n->Unable to fetch data\n->Might be a issue with your city name or the API key.\n->Please check")
+        weather_forcast(input("\nEnter city name in the format (city,name)\nAns:"),Api)
+        weather_forcast(city,API)
