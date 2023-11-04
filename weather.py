3bebc885b50be78e597acee6a35f589574fdf511
MrRohanBatra <rohanbatra.in@gmail.com>
2023-11-04 19:40:48 +0530

    update weather.py

--- weather.py
+++ weather.py
@@ -1,27 +1,29 @@
+#!/usr/bin/python3
 import requests
 import os
 from datetime import *
 import platform
+#All modules imported
 if (os.name=="nt"):    
-    import py_setenv as pyset
+    import py_setenv as pyset #importing py_setenv module for permanently saving the environment variable
     if ("API_Weatherapi.com" in os.environ):
-        API=os.getenv("API_weatherapi.com")
+        API=os.getenv("API_weatherapi.com") #got API key from system only
     else:
         API=input("Enter your API key: ")
         pyset.set_variable("API_Weatherapi.com",API,os.getenv("USERNAME"))   # to set environment variable
 else:
     API=input("Enter your API key: ")
-print("\nutorefresh mode is set to on")
-city=input("\nEnter city name in the format (city,country)\nAns:")
+print("\nauto refresh mode is set to on")
+city=input("\nEnter city name in the format (city,country)\nAns:") #input by the user
 if (os.name=="nt"):
     os.system("cls")
 else:
     os.system("clear")
 if (city.lower()=="delhi"):
     city="delhi,india"
-def autorefresh(city,api,t):
+def autorefresh(city,api,t): #function for autorefreshing the weather forecast function
     while True:
-        if (int((datetime.now().time().strftime("%S")))- int(t) > 5):
+        if (int((datetime.now().time().strftime("%S")))- int(t) > 5): #timer for 15 seconds
             if (os.name=="nt"):
                 os.system("cls")
                 print("Refreshed......\n")
@@ -40,15 +42,15 @@ def weather_forcast(city,Api):
     baseurl="http://api.weatherapi.com/v1/current.json"
     p={'key':Api,'q':city,'aqi':"yes"}
     fetch_data=requests.get(baseurl,params=p)
-    if (fetch_data.status_code ==200):
+    if (fetch_data.status_code ==200): #if data is fetched successfully
         data=fetch_data.json()
-        temp_c=data["current"]["temp_c"]
-        temp_f=data["current"]["temp_f"]
-        time=datetime.now().time().strftime("%H:%M")
-        location=data["location"]["name"]+","+data["location"]["country"]
+        temp_c=data["current"]["temp_c"] #temperature in celcious
+        temp_f=data["current"]["temp_f"] #tempereture in farenhiet
+        time=datetime.now().time().strftime("%H:%M") #time using datetime module and formatted string using stftime
+        location=data["location"]["name"]+","+data["location"]["country"] 
         print(f"\nThe weather for the day is..................\n->Location:{location}\n->Temperature:{temp_c}°C || {temp_f}°F\n->Date:{datetime.now().date()} {time}\n\n")
-    else:
+    else: #if data is not fetched successfully
         print("\n->Unable to fetch data\n->Might be a issue with your city name or the API key.\n->Please check")
-        weather_forcast(input("\nEnter city name in the format (city,country)\nAns:"),Api)
-    autorefresh(city,Api,datetime.now().time().strftime("%S"))
-weather_forcast(city,API)
+        weather_forcast(input("\nEnter city name in the format (city,country)\nAns:"),input("\nEnter API KEY:")) #recall the function with new api key and new city name 
+    autorefresh(city,Api,datetime.now().time().strftime("%S")) #calling the autorefresh function
+weather_forcast(city,API) #main function call
