d31a74a6274b04e07f40aa6170babad120162381
MrRohanBatra <rohanbatra.in@gmail.com>
2023-11-05 20:47:17 +0530

    update weather.py

--- weather.py
+++ weather.py
@@ -11,11 +11,48 @@ if (os.name=="nt"):
         pyset.set_variable("API_Weatherapi.com",API,os.getenv("USERNAME"))   # to set environment variable
 else:
     API=input("Enter your API key: ")
-print("autorefresh mode is set to on")
-city=input("Enter city name in the format (city,country)\nAns:")
-os.system("cls")
-if (city.lower()=="delhi"):
-    city="delhi,india"
+l_city=[
+    ("Mumbai", "India"),
+    ("Delhi", "India"),
+    ("Kolkata", "India"),
+    ("Chennai", "India"),
+    ("Bangalore", "India"),
+    ("Hyderabad", "India"),
+    ("Ahmedabad", "India"),
+    ("Pune", "India"),
+    ("Surat", "India"),
+    ("Jaipur", "India"),
+    ("Lucknow", "India"),
+    ("Kanpur", "India"),
+    ("Nagpur", "India"),
+    ("Visakhapatnam", "India"),
+    ("Indore", "India"),
+    ("Patna", "India"),
+    ("Vadodara", "India"),
+    ("Ghaziabad", "India"),
+    ("Ludhiana", "India"),
+    ("Nashik", "India"),
+    ("Faridabad", "India"),
+    ("Agra", "India"),
+    ("Meerut", "India"),
+    ("Varanasi", "India"),
+    ("Srinagar", "India"),
+    ("Amritsar", "India"),
+    ("Jabalpur", "India"),
+    ("Aurangabad", "India"),
+    ("Dhanbad", "India"),
+    ("Prayagraj", "India"),
+    ("Ranchi", "India"),
+    ("Bhopal", "India"),
+    ("Gwalior", "India"),
+    ("Tiruchirappalli", "India"),
+    ("Kota", "India"),
+    ("Vasai-Virar", "India"),
+    ("Kollam", "India"),
+    ("Bhiwandi", "India"),
+    ("Jamshedpur", "India"),
+    ("Asansol", "India")
+]      
 def autorefresh(city,api,t):
     while True:
         if (int((datetime.now().time().strftime("%S")))- int(t) > 30):
@@ -31,7 +68,7 @@ def autorefresh(city,api,t):
                 weather_forcast(city,api)
         else:
             continue
-def weather_forcast(city,Api):
+def weather_forcast(city,Api,s=0):
     baseurl="http://api.weatherapi.com/v1/current.json"
     p={'key':Api,'q':city,'aqi':"yes"}
     fetch_data=requests.get(baseurl,params=p)
@@ -45,5 +82,31 @@ def weather_forcast(city,Api):
     else:
         print("\n->Unable to fetch data\n->Might be a issue with your city name or the API key.\n->Please check")
         weather_forcast(input("\nEnter city name in the format (city,country)\nAns:"),Api)
-    autorefresh(city,Api,datetime.now().time().strftime("%S"))
-weather_forcast(city,API)
+    if(s==0):
+        autorefresh(city,Api,datetime.now().time().strftime("%S"))
+def mainmenu():
+    global API
+    print("        Welcome to the weather app.........................          ")
+    print("\nTo display the weather")
+    while True:
+        print("Choose any one from the following options\n1->city of your choice\n2->Cities from favourate list\n3->exit")
+        i=int(input("Enter your Choice"))
+        if(i==1):
+            print("autorefresh mode is set to on")
+            city=input("Enter city name in the format (city,country)\nAns:")
+            os.system("cls")
+            if (city.lower()=="delhi"):
+                city="delhi,india"
+            weather_forcast(city,API,s=0)
+        elif(i==2):
+            global l_city
+            l=l_city
+            for i in l:
+                city=i[0]+","+i[1]
+                print(city)
+                weather_forcast(city,API,s=1)
+                print("\n")
+        elif(i==3):
+            break
+    exit()   
+mainmenu()
